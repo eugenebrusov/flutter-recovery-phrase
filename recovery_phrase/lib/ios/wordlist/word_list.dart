2c22530a634b1545ae845551907993c4daea8a0f
eugenebrusov <eugenebrusov@gmail.com>
2021-08-10 14:47:23 +0200

    ui updates

--- recovery_phrase/lib/ios/wordlist/word_list.dart
+++ recovery_phrase/lib/ios/wordlist/word_list.dart
@@ -9,19 +9,36 @@ class WordList extends StatelessWidget {
     return CupertinoPageScaffold(
       child: SafeArea(
         child: Container(
-          padding: const EdgeInsets.only(left: 24.0, right: 24.0, top: 60.0, bottom: 16.0),
+          padding: const EdgeInsets.only(left: 18.0, right: 18.0, top: 60.0, bottom: 16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Text(
-                  AppLocalizations.writePhrase,
-                  style: Theme.CupertinoTheme.of(context).textTheme.largeTitleTextStyle
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: Text(
+                    AppLocalizations.writePhrase,
+                    style: Theme.CupertinoTheme.of(context).textTheme.largeTitleTextStyle
+                ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 20.0),
+                padding: const EdgeInsets.only(top: 20.0, left: 6.0, right: 6.0),
                 child: Text(
                     AppLocalizations.phraseDescription,
                     style: Theme.CupertinoTheme.of(context).textTheme.title1TextStyle
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: Container(
+                  padding: const EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                      color: Color(0xFF1B2540),
+                      borderRadius: BorderRadius.all(Radius.circular(8.0))
+                  ),
+                  child: Text(
+                      AppLocalizations.phraseDescription,
+                      style: Theme.CupertinoTheme.of(context).textTheme.title1TextStyle
+                  ),
                 ),
               ),
               Expanded(
